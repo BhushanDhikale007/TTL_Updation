5056db66855d8f7cb9956a24d963ad166ee211da
BhushanDhikale007 <101262292+BhushanDhikale007@users.noreply.github.com>
2022-03-25 17:13:07 +0530

    Add files via upload

---
+++ create.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://bhushan111:bhushan111@cluster0.hv2f2.mongodb.net/mongod?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	mongodDatabase := client.Database("mongod")
+	TenantsCollection := mongodDatabase.Collection("Tenants")
+
+	TenantsResult, err := TenantsCollection.InsertMany(ctx, []interface{}{
+		bson.D{
+			{"title", "GraphQL for API Development"},
+			{"description", "Learn about GraphQL from the co-creator of GraphQL, Lee Byron."},
+			{"duration", 25},
+		},
+		bson.D{
+
+			{"title", "Progressive Web Application Development"},
+			{"description", "Learn about PWA development with Tara Manicsic."},
+			{"duration", 32},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Inserted %v documents into Tenants collection!\n", len(TenantsResult.InsertedIDs))
+}
